b5ade340bb51f5a636178cb373872651c02dfc26
Rodrigo Silveira <r3850365@gmail.com>
2026-06-08 13:03:12 -0700

    Add settlement preview endpoint

--- backend/internal/http/routes/collaborators.go
+++ backend/internal/http/routes/collaborators.go
@@ -7,6 +7,7 @@ func RegisterCollaboratorRoutes(v1 fiber.Router, deps Dependencies) {
 	r.Get("/", deps.CollaboratorHandler.List)
 	r.Post("/", deps.CollaboratorHandler.Create)
 	r.Get("/:id", deps.CollaboratorHandler.GetByID)
+	r.Get("/:collaboratorId/settlement-preview", deps.CurrentAccountHandler.SettlementPreview)
 	r.Get("/:collaboratorId/current-account", deps.CurrentAccountHandler.GetDetail)
 	r.Get("/:collaboratorId/ledger-entries", deps.CurrentAccountHandler.ListEntries)
 }
